5b76cdc75cb044b080b575b623ba65f8c62f38a4
Mohit Singh <mohitsinghx12.com>
2024-12-23 04:47:19 +1030

    deploy issues fixed

--- src/pages/explore/sidebar-menu/getting-started/getting-started.page.tsx
+++ src/pages/explore/sidebar-menu/getting-started/getting-started.page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function GettingStartedPage() {
   return <div>Getting Started</div>;
 }
--- src/pages/explore/sidebar-menu/templates/templates.page.tsx
+++ src/pages/explore/sidebar-menu/templates/templates.page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function TemplatesPage() {
   return <div>All Templates</div>;
 }
